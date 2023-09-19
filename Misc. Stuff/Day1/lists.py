e37fce41cfb75694f8df50ffe92d5e2e5686e6e8
citrus3301 <57966199+citrus3301@users.noreply.github.com>
2023-09-19 17:08:46 -0400

    Misc. Stuff
    Added some small files i just used to learn basic things

---
+++ Misc. Stuff/Day1/lists.py
@@ -0,0 +1,9 @@
+friends = ["Kevin", "2", "True", "Blah", "Train", "Haha"]
+#		0	1	2		3	4		5
+#		-6	-5	-4		-3	-2	-1
+print (friends[-3] + friends[2])
+print (friends[:-1])
+print (friends[1:4])
+print (friends[-4:-1])
+friends[2] = input("Give me a new thing for this: ")
+print (friends[2])
---
+++ Misc. Stuff/Day1/madLibs.py
@@ -0,0 +1,4 @@
+Adj1 = input("Input an adjective: ")
+Noun1 = input("Input a noun: ")
+PluralNoun1 = input("Input a plural noun: ")
+print("There are many " + Adj1 + " ways to chose a/an " + Noun1 + " to read. First you could ask for a reccomendation from your friends and " + PluralNoun1 + ".")
